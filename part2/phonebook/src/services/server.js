e8301028de67716d6fce3bb27e58f4ce2b1da850
Alejo-end <alejandro.deleon@utp.ac.pa>
2022-07-17 22:14:25 +0300

    part3.12  build wrap up

--- part2/phonebook/src/services/server.js
+++ part2/phonebook/src/services/server.js
@@ -1,29 +1,24 @@
 import axios from 'axios'
-const url = 'https://backend-fullstackcourse.herokuapp.com'
+const url = '/api/persons'
 
-//as instructed in 2.16, similar as what was shown in the example. Due to a react warning I assigned the default exports to a variable first.
-const getAllPersons = async () => {
-    const request = axios.get(url + '/api/persons')
-    const response = await request
-    return response.data
+const getAllPersons = () => {
+    const request = axios.get(url)
+    return request.then(response => response.data)
 }
 
-const createPerson = async newPerson => {
-    const request = axios.post(url + '/api/persons', newPerson)
-    const response = await request
-    return response.data
+const createPerson = newPerson => {
+    const request = axios.post(url, newPerson)
+    return request.then(response => response.data)
 }
 
-const updatePerson = async (id, newPerson) => {
-    const request = axios.put(`${url} + '/api/persons/' + ${id}`, newPerson)
-    const response = await request
-    return response.data
+const updatePerson = (id, newPerson) => {
+    const request = axios.put(`${url}/${id}`, newPerson)
+    return request.then(response => response.data)
 }
 
-const deletePerson = async (id) => {
-    const request = axios.delete(`${url} + '/api/persons/' + ${id}`)
-    const response = await request
-    return response.data
+const deletePerson = (id) => {
+    const request = axios.delete(`${url}/${id}`)
+    return request.then(response => response.data)
 }
 
 const server = { getAllPersons, createPerson, updatePerson, deletePerson }
