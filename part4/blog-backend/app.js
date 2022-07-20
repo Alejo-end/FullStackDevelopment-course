2761566c56cef41216d6dc6a45225d7bfe0a7048
Alejo-end <alejandro.deleon@utp.ac.pa>
2022-07-20 16:54:44 +0300

    adding test helper functions part4.3-part4.5

---
+++ part4/blog-backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require("./utils/config")
+const blogsRouter = require('./controllers/blogs')
+const logger = require("./utils/logger")
+const middleware = require("./utils/middleware")
+
+logger.info('Connecting to:', config.MONGOURL)
+mongoose.connect(config.MONGOURL)
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ part4/blog-backend/utils/list_helper.js
@@ -0,0 +1,25 @@
+const dummy = () => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+
+    return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
+const favouriteBlog = (blogs) => {
+    let fav = blogs.reduce((sum, item) => {
+        return sum.likes > item.likes ? sum : item
+    })
+
+    return [fav.title, fav.author, fav.likes]
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favouriteBlog
+}
