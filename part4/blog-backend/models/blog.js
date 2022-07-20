fac51fdbec405a507d0b961fdcf1d41333307d21
Alejo-end <alejandro.deleon@utp.ac.pa>
2022-07-20 14:42:31 +0300

    file structure for backend part4.2

---
+++ part4/blog-backend/models/blog.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.__v
+    }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+module.exports = Blog
