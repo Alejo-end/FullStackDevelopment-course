9e57b2e69072ca345e08c5f3456c9816fa51afc4
Alejo-end <alejandro.deleon@utp.ac.pa>
2022-07-21 06:16:31 +0300

    part4.9-part4.10 finished

--- part4/blog-backend/tests/api.test.js
+++ part4/blog-backend/tests/api.test.js
@@ -6,6 +6,13 @@ const Blog = require("../models/blog")
 
 const api = supertest(app)
 
+const oneBlog = {
+    title: "React patterns 2",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+}
+
 beforeEach(async () => { //similar to the tutorial of part 4 "Testing the backend"
     await Blog.deleteMany({})
     const testBlogs = blogs.map(blog => new Blog(blog)) 
@@ -25,6 +32,29 @@ describe('GET method', () => {
         const response = await api.get('/api/blogs')
         expect(response.body.length).toBe(blogs.length)
     })    
+    test('id of blogs', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].id).toBeDefined()
+    })
+})
+
+describe('POST method', () => {
+    test('Check if blog can be added', async () => { 
+
+        await api.post('/api/blogs').send(oneBlog).expect(201).expect('Content-Type', /application\/json/) 
+        const reply = await api.get('/api/blogs')
+        expect(reply.body).toHaveLength(blogs.length + 1) 
+
+        const titles = reply.body.map(blog => blog.title) 
+        const authors = reply.body.map(blog => blog.author)
+        const urls = reply.body.map(blog => blog.url)
+        const likes = reply.body.map(blog => blog.likes)
+
+        expect(titles).toContain("React patterns 2")
+        expect(authors).toContain("Michael Chan")
+        expect(urls).toContain("https://reactpatterns.com/")
+        expect(likes).toContain(7)
+    })
 })
 
 afterAll(() => {
